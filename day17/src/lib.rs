4363d3b46f22c689e2e75c93058695fbceb320d0
Marcello Cordeiro <23259818+marcellocordeiro@users.noreply.github.com>
2023-12-17 23:31:41 -0300

    refactor day17 solutions

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -1,13 +1,23 @@
+use pathfinding::directed::dijkstra::dijkstra;
+
 pub const INPUT: &str = include_str!("inputs/input.txt");
 pub const SAMPLE1: &str = include_str!("inputs/sample1.txt");
 pub const SAMPLE2: &str = include_str!("inputs/sample2.txt");
 
 pub type Grid = Vec<Vec<usize>>;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct Crucible {
+    position: Position,
+    direction: Direction,
+    remaining: usize,
+    weight: usize,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Position(usize, usize);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Direction {
     North,
     South,
@@ -48,6 +58,108 @@ impl Direction {
             West => North,
         }
     }
+}
+
+fn possible_directions(
+    c: &Crucible,
+    min_steps: usize,
+    max_steps: usize,
+) -> Vec<(Direction, usize)> {
+    let mut possible = vec![];
+
+    if c.remaining <= (max_steps - min_steps) {
+        possible.push((c.direction.turn_left(), max_steps));
+        possible.push((c.direction.turn_right(), max_steps));
+    }
+
+    if c.remaining > 0 {
+        possible.push((c.direction, c.remaining));
+    }
+
+    possible
+}
+
+fn next_crucibles(
+    c: &Crucible,
+    grid: &Grid,
+    min_steps: usize,
+    max_steps: usize,
+) -> Vec<(Crucible, usize)> {
+    let possible = possible_directions(c, min_steps, max_steps);
+
+    possible
+        .into_iter()
+        .filter_map(|(direction, remaining)| {
+            let Position(i, j) = c.position;
+
+            let offset = direction.offset();
+            let new_i = i.checked_add_signed(offset.0)?;
+            let new_j = j.checked_add_signed(offset.1)?;
+
+            let row = grid.get(new_i)?;
+            let weight = *row.get(new_j)?;
+
+            Some(Crucible {
+                position: Position(new_i, new_j),
+                direction,
+                remaining: remaining - 1,
+                weight,
+            })
+        })
+        .map(|c| {
+            let weight = c.weight;
+            (c, weight)
+        })
+        .collect()
+}
+
+fn get_shortest_path(grid: &Grid, min_steps: usize, max_steps: usize) -> usize {
+    let starting_crucibles = [
+        Crucible {
+            position: Position(0, 0),
+            direction: Direction::East,
+            remaining: max_steps,
+            weight: 0,
+        },
+        Crucible {
+            position: Position(0, 0),
+            direction: Direction::South,
+            remaining: max_steps,
+            weight: 0,
+        },
+    ];
+
+    let successors = |c: &Crucible| next_crucibles(c, grid, min_steps, max_steps);
+
+    let end_position = Position(grid.len() - 1, grid[0].len() - 1);
+    let success =
+        |c: &Crucible| c.position == end_position && c.remaining <= (max_steps - min_steps);
+
+    let results = starting_crucibles
+        .into_iter()
+        .map(|start| dijkstra(&start, successors, success).unwrap())
+        .collect::<Vec<_>>();
+
+    /*let visited = &results[0].0;
+    let result = results[0].1;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if visited
+                .iter()
+                .find(|c| c.position == Position(i, j))
+                .is_some()
+            {
+                print!("*");
+            } else {
+                print!(".");
+            }
+        }
+
+        println!();
+    }*/
+
+    usize::min(results[0].1, results[1].1)
 }
 
 pub fn parse_input(input: &str) -> Grid {
--- day17/src/part1.rs
+++ day17/src/part1.rs
@@ -1,106 +1,12 @@
-use crate::{parse_input, Direction, Grid, Position};
-use pathfinding::directed::dijkstra::dijkstra;
+use crate::{get_shortest_path, parse_input};
+
+const MIN_STEPS: usize = 0;
+const MAX_STEPS: usize = 3;
 
 pub fn result(input: &str) -> usize {
     let grid = parse_input(input);
 
-    get_shortest_path(&grid)
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Crucible {
-    position: Position,
-    direction: Direction,
-    remaining: usize,
-    weight: usize,
-}
-
-impl Crucible {
-    fn next_crucibles(&self, grid: &Grid) -> Vec<(Self, usize)> {
-        [
-            (self.direction.turn_left(), 3),
-            (self.direction.turn_right(), 3),
-            (self.direction, self.remaining),
-        ]
-        .into_iter()
-        .filter_map(|(dir, remaining)| {
-            if remaining == 0 {
-                return None;
-            }
-
-            let Position(i, j) = self.position;
-
-            let offset = dir.offset();
-            let new_i = i.checked_add_signed(offset.0)?;
-            let new_j = j.checked_add_signed(offset.1)?;
-
-            let row = grid.get(new_i)?;
-            let weight = *row.get(new_j)?;
-
-            Some(Self {
-                position: Position(new_i, new_j),
-                direction: dir,
-                remaining: remaining - 1,
-                weight,
-            })
-        })
-        .map(|crucible| {
-            let weight = crucible.weight;
-            (crucible, weight)
-        })
-        .collect()
-    }
-}
-
-pub fn get_shortest_path(grid: &Grid) -> usize {
-    let start_crucible_1 = Crucible {
-        position: Position(0, 0),
-        direction: Direction::East,
-        remaining: 3,
-        weight: 0,
-    };
-
-    let start_crucible_2 = Crucible {
-        position: Position(0, 0),
-        direction: Direction::South,
-        remaining: 3,
-        weight: 0,
-    };
-
-    let end_position = Position(grid.len() - 1, grid[0].len() - 1);
-
-    let results = [start_crucible_1, start_crucible_2]
-        .into_iter()
-        .map(|c| {
-            dijkstra(
-                &c,
-                |c| c.next_crucibles(grid),
-                |c| c.position == end_position,
-            )
-            .unwrap()
-        })
-        .collect::<Vec<_>>();
-
-    /*let visited = &results[0].0;
-    let result = results[0].1;
-
-    for i in 0..grid.len() {
-        for j in 0..grid[0].len() {
-            if visited
-                .iter()
-                .find(|c| c.position == Position(i, j))
-                .is_some()
-            {
-                print!("*");
-            } else {
-                print!(".");
-            }
-        }
-
-        println!();
-    }*/
-
-    usize::min(results[0].1, results[1].1)
+    get_shortest_path(&grid, MIN_STEPS, MAX_STEPS)
 }
 
 #[cfg(test)]
